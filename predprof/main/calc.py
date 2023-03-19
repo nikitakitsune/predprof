2499b6f5737587d4cf27c89c4b1b3e08b6bd0b83
skewb1k <artem.krinitsyn@gmail.com>
2023-03-19 13:02:04 +0300

    v1.2 calc

--- predprof/main/calc.py
+++ predprof/main/calc.py
@@ -1,1 +1,22 @@
 import math
+
+vmax = 2
+oxi = 0
+
+
+def get_v(w, m):
+    return vmax * (w // 80) * (200 // m)
+
+
+def get_gn(go, kp):
+    return go * (1 + kp)
+
+
+def get_kp(t):
+    return math.sin(math.pi / 2 + math.pi * (t + 0.5 * oxi) / 40)
+
+
+def get_e(t):
+    return sum(range(t + 1))
+
+print(math.sin())
